fd749ee3067ca00f9d63629d8da9ed9ef02cb2d3
kento980037 <kento_0037@yahoo.co.jp>
2025-06-22 17:33:37 +0530

    add basic component

--- src/screens/app.tsx
+++ src/screens/app.tsx
@@ -1,14 +1,22 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, type ImageSourcePropType, View } from "react-native";
 import { registerRootComponent } from "expo";
 import { StatusBar } from "expo-status-bar";
+import { ImageViewer } from "@/components/image-viewer";
+import { Button } from "@/components/button";
+
+const PlaceholderImage =
+  require("@/assets/images/background-image.png") as ImageSourcePropType;
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={{ color: "#fff" }}>Hello, World!</Text>
-      <Text style={styles.text}>
-        Open up app.tsx to start working on your app!
-      </Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="写真を選択" theme="primary" />
+        <Button label="この写真を使用" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,10 +27,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#25292d",
     alignItems: "center",
-    justifyContent: "center",
   },
-  text: {
-    color: "#fff",
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
 
